bbfaaf0a4e4b99bf2e301d493d5224cc3c5c271c
EvanMad <evan.madurai@gmail.com>
2025-05-28 11:44:51 +0100

    ping and arm commands added

---
+++ src/steady/commands/arm.rs
@@ -0,0 +1,25 @@
+use super::command::Command;
+
+pub struct ArmCommand {
+
+}
+
+impl ArmCommand {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Command for ArmCommand {
+    fn to_string(&self) -> String {
+        return "startf\n".to_string();
+    }
+
+    fn from_string(command: &str) -> Result<Self, String> {
+        if command != "arm\n" {
+            return Err("Invalid command format: expected 'arm\\n'".to_string());
+        }
+        
+        return Ok(ArmCommand {});
+    }
+}
--- src/steady/commands/mod.rs
+++ src/steady/commands/mod.rs
@@ -1,2 +1,4 @@
 pub mod start;
 pub mod command;
+pub mod arm;
+pub mod ping;
